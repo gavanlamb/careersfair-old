7250466b81ec3eba21ad1563e03409941c90cac4
gjl1988 <gavan.jon.lamb@gmail.com>
2015-06-02 03:34:57 +1200

    Before revert

---
+++ careersfair/Controllers/FormNameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace careersfair.Controllers
+{
+    public class FormNameController : Controller
+    {
+        // GET: FormName
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
