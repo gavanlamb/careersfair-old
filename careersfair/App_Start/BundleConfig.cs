b6d4dc88f58d26b578e892ae1678a5384d521118
gjl1988 <gavan.jon.lamb@gmail.com>
2015-06-03 23:22:39 +1200

    revert

--- careersfair/App_Start/BundleConfig.cs
+++ careersfair/App_Start/BundleConfig.cs
@@ -10,7 +10,7 @@ namespace careersfair.App_Start
             //Script bundle for the ribbon menu
             bundles.Add(new ScriptBundle("~/Bundles/ribbon").Include("~/Scripts/jquery-2.0.0.min.js", "~/Scripts/jquery.smartWizard.js"));
             //CSS bundle for the ribbon menu
-            bundles.Add(new StyleBundle("~/Content/style").Include("~/Content/css/ribbon_smart_wizard.css", "~/Content/css/menu.css", "~/Content/css/Site.css"));
-        }
+            bundles.Add(new StyleBundle("~/Content/style").Include("~/Content/css/ribbon_smart_wizard.css", "~/Content/css/menu.css"));
+        } 
     }
 }
