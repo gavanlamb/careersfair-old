cddc2d3c51c9e21a0b96623b014b9dc0d92cbf92
gjl1988 <gavan.jon.lamb@gmail.com>
2015-06-03 22:58:49 +1200

    Before revert

--- careersfair/App_Start/BundleConfig.cs
+++ careersfair/App_Start/BundleConfig.cs
@@ -8,15 +8,21 @@ namespace careersfair.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             //Script bundle for the ribbon menu
-            bundles.Add(new ScriptBundle("~/Bundles/ribbon").Include("~/Scripts/jquery-2.0.0.min.js", "~/Scripts/jquery.smartWizard.js"));
-           //Script for FormBuilder
-            bundles.Add(new ScriptBundle("~/Bundles/formbuilder").Include("~/Plugin/FormBuilder/assests/js/main-built.js",
-                "~/Plugin/FormBuilder/assests/js/lib/require.js"));
+            bundles.Add(new ScriptBundle("~/Bundles/ribbon").Include(
+                "~/Scripts/jquery-2.0.0.min.js", 
+                "~/Scripts/jquery.smartWizard.js"));
+            //Script for FormBuilder
+            bundles.Add(new ScriptBundle("~/Bundles/formbuilder").Include(
+                "~/Scripts/main-built.js",
+                "~/Scripts/lib/require.js"));
             //CSS bundle for the ribbon menu
-            bundles.Add(new StyleBundle("~/Content/style").Include("~/Content/css/ribbon_smart_wizard.css", "~/Content/css/menu.css", "~/Content/css/Site.css"));
-            //CSS bundle for FormBuilder
-            bundles.Add(new StyleBundle("~/Content/styles").Include("~/Plugin/FormBuilder/assests/css/lib/bootstrap.min.css",
-                "~/Plugin/FormBuilder/assests/css/lib/bootstrap-responsive.min.css","~/Plugin/FormBuilder/assests/css/custom.css"));
+            bundles.Add(new StyleBundle("~/Content/style").Include(
+                "~/Content/css/ribbon_smart_wizard.css", 
+                "~/Content/css/menu.css", 
+                "~/Content/css/Site.css",
+                "~/Content/css/bootstrap.min.css",
+                "~/Content/FormBuilder/assests/css/bootstrap-responsive.min.css",
+                "~/Content/FormBuilder/assests/css/custom.css"));
         } 
     }
 }
