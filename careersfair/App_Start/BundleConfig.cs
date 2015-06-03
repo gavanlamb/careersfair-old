cfe26e2faf513e3b2a2a3303e5e17f2c47c29ab5
saiPA <saipraneeth_a@hotmail.com>
2015-06-03 21:48:50 +1200

    adding formbuilder
    Adding form builder to step 3

--- careersfair/App_Start/BundleConfig.cs
+++ careersfair/App_Start/BundleConfig.cs
@@ -9,8 +9,14 @@ namespace careersfair.App_Start
         {
             //Script bundle for the ribbon menu
             bundles.Add(new ScriptBundle("~/Bundles/ribbon").Include("~/Scripts/jquery-2.0.0.min.js", "~/Scripts/jquery.smartWizard.js"));
+           //Script for FormBuilder
+            bundles.Add(new ScriptBundle("~/Bundles/formbuilder").Include("~/Plugin/FormBuilder/assests/js/main-built.js",
+                "~/Plugin/FormBuilder/assests/js/lib/require.js"));
             //CSS bundle for the ribbon menu
             bundles.Add(new StyleBundle("~/Content/style").Include("~/Content/css/ribbon_smart_wizard.css", "~/Content/css/menu.css", "~/Content/css/Site.css"));
+            //CSS bundle for FormBuilder
+            bundles.Add(new StyleBundle("~/Content/styles").Include("~/Plugin/FormBuilder/assests/css/lib/bootstrap.min.css",
+                "~/Plugin/FormBuilder/assests/css/lib/bootstrap-responsive.min.css","~/Plugin/FormBuilder/assests/css/custom.css"));
         } 
     }
 }
